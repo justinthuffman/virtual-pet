25beb30af9a44255b8b12f9bb22503b6c56c1784
Justin Huffman <justin.t.huffman@gmail.com>
2018-09-16 21:50:36 -0400

    Working pet --no testing.

--- src/main/java/virtualPet/VirtualPet.java
+++ src/main/java/virtualPet/VirtualPet.java
@@ -2,9 +2,116 @@ package virtualPet;
 
 public class VirtualPet {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// Traits of virtual pet
+	int hunger;
+	int thirst;
+	int waste;
+	int boredom;
+	int tiredness;
 
+	public VirtualPet(int initialHunger, int initialThirst, int initialWaste, int initialBoredom,
+			int initialTiredness) {
+		this.hunger = initialHunger;
+		this.thirst = initialThirst;
+		this.waste = initialWaste;
+		this.boredom = initialBoredom;
+		this.tiredness = initialTiredness;
 	}
 
+	public String userInputChoices() {
+		return ("What would you like to do:\n1. Feed\n2. Water\n3. Let out\n4. Play with\n5. Send to bed");
+	}
+
+	public void eat() {
+		hunger -= 5;
+		thirst += 1;
+		waste += 2;
+		boredom += 1;
+		tiredness += 1;
+	}
+
+	public void drink() {
+		hunger += 1;
+		thirst -= 5;
+		waste += 2;
+		boredom += 1;
+		tiredness += 1;
+	}
+
+	public void bathroom() {
+		hunger += 1;
+		thirst += 1;
+		waste -= 5;
+		boredom += 1;
+		tiredness += 1;
+	}
+
+	public void play() {
+		hunger += 2;
+		thirst += 2;
+		waste += 0;
+		boredom -= 5;
+		tiredness += 2;
+	}
+
+	public void sleep() {
+		hunger += 2;
+		thirst += 2;
+		waste += 2;
+		boredom += 0;
+		tiredness -= 5;
+	}
+
+	public String statusUpdate() {
+		if (hunger > 3) {
+			return "               )\\._.,--....,'``.       \n" + " .b--.        /;   _.. \\   _\\  (`._ ,. \n"
+					+ "`=,-,-'~~~   `----(,_..'--(,_..'`-.;.'\n \n Your cat is hungry!\n";
+		} else if (thirst > 3) {
+			return "    (\"`-''-/\").___..--''\"`-._\n" + "     `6_ 6  )   `-.  (     ).`-.__.`)\n"
+					+ "     (_Y_.)'  ._   )  `._ `. ``-..-'\n" + "   _..`--'_..-_/  /--'_.' ,'\n"
+					+ "  (il).-''  (li).'  ((!.-' \n--Your cat is thirsty!\n";
+		} else if (waste > 3) {
+			return "                       (`.-,')\n" + "                     .-'     ;\n"
+					+ "                 _.-'   , `,-\n" + "           _ _.-'     .'  /._\n"
+					+ "         .' `  _.-.  /  ,'._;)\n" + "        (       .  )-| (\n"
+					+ "         )`,_ ,'_,'  \\_;)  \n" + " ('_  _,'.'  (___,))\n" + "  `-:;.-'\n"
+					+ "\n --Everybody has to poop, and so does your cat... like right now.\n";
+		} else if (tiredness > 3) {
+			return "   |\\      _,,,---,,_\n" + "   /,`.-'`'    -.  ;-;;,_\n" + "  |,4-  ) )-,_..;\\ (  `'-'\n"
+					+ " '---''(_/--'  `-'\\_) \n--Your cat seems sleepy.\n";
+		} else
+			return "                                 _\n" + "                                | \\\n"
+					+ "                                | |\n" + "                                | |\n"
+					+ "           |\\                   | |\n" + "          /, ~\\                / /\n"
+					+ "         X     `-.....-------./ /\n" + "          ~-. ~  ~              |\n"
+					+ "             \\             /    |\n" + "              \\  /_     ___\\   /\n"
+					+ "              | /\\ ~~~~~   \\ |\n" + "              | | \\        || |\n"
+					+ "              | |\\ \\       || )\n" + "             (_/ (_/      ((_/";
+	}
+
+	boolean notDead() {
+		if (hunger > 10 || thirst > 10 || waste > 10 || boredom > 10 || tiredness > 10) {
+			return false;
+		}
+		return true;
+	}
+
+	String death() {
+		if (hunger > 10) {
+			return "Your cat starved to death!";
+		}
+		if (thirst > 10) {
+			return "Your cat died of dehydration!";
+		}
+		if (waste > 10) {
+			return "Your cat ran away to shit and never came back!";
+		}
+		if (boredom > 10) {
+			return "Your cat didn't die of boredom, but it did find a better owner that plays with them and is now way happier.";
+		}
+		if (tiredness > 10) {
+			return "Your cat died of exhaustion!";
+		} else
+			return "";
+	}
 }
--- src/main/java/virtualPet/VirtualPetApp.java
+++ src/main/java/virtualPet/VirtualPetApp.java
@@ -1,10 +1,97 @@
 package virtualPet;
 
+import java.util.Scanner;
+
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		VirtualPet myVirtualPetInstance = new VirtualPet(0, 0, 0, 0, 0);
+
+		System.out.println("            _             _\n" + "           | '-.       .-' |\n"
+				+ "            \\'-.'-\"\"\"-'.-'/    _\n" + "             |= _:'.':_ =|    /:`)\n"
+				+ "             \\ <6>   <6> /   /  /\n" + "             |=   |_|   =|   |:'\\\n"
+				+ "             >\\:.  \"  .:/<    ) .|\n" + "              /'-._^_.-'\\    /.:/\n"
+				+ "             /::.     .::\\  /' /\n" + "           .| '::.  .::'  |;.:/\n"
+				+ "          /`\\:.         .:/`\\(\n" + "         |:. | ':.   .:' | .:|\n"
+				+ "         | ` |:.;     ;.:| ` |\n" + "          \\:.|  |:. .:|  |.:/\n"
+				+ "           \\ |:.|     |.:| /\n" + "           /'|  |\\   /|  |`\\\n"
+				+ "          (,,/:.|.-'-.|.:\\,,)\n" + "            (,,,/     \\,,,) \nWelcome to your virtual cat!");
+
+		String petAction;
+		while (myVirtualPetInstance.notDead()) {
+			do {
+				System.out.println(myVirtualPetInstance.userInputChoices());
+				petAction = input.nextLine();
+			} while (!petAction.equals("1") && !petAction.equals("2") && !petAction.equals("3")
+					&& !petAction.equals("4") && !petAction.equals("5"));
+			switch (petAction) {
+			case "1":
+				System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n");
+				System.out.println("Mmmmmm mmmmm good. They love your cooking.");
+				myVirtualPetInstance.eat();
+				if (myVirtualPetInstance.notDead() == true) {
+					System.out.println("");
+					System.out.println(myVirtualPetInstance.statusUpdate());
+				}
+				break;
+
+			case "2":
+				System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n");
+				System.out.println("They drank SO MUCH!");
+				myVirtualPetInstance.drink();
+				if (myVirtualPetInstance.notDead() == true) {
+					System.out.println("");
+					System.out.println(myVirtualPetInstance.statusUpdate());
+				}
+				break;
+
+			case "3":
+				System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n");
+				System.out.println("They look so relieved!");
+				myVirtualPetInstance.bathroom();
+				if (myVirtualPetInstance.notDead() == true) {
+					System.out.println("");
+					System.out.println(myVirtualPetInstance.statusUpdate());
+				}
+				break;
+
+			case "4":
+				System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n");
+				System.out.println("All work and no play makes Homer something something");
+				myVirtualPetInstance.play();
+				if (myVirtualPetInstance.notDead() == true) {
+					System.out.println("");
+					System.out.println(myVirtualPetInstance.statusUpdate());
+				}
+				break;
+
+			case "5":
+				System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n");
+				System.out.println("Don't let the bedbugs bite (unless you're into that sort of thing).");
+				myVirtualPetInstance.sleep();
+				if (myVirtualPetInstance.notDead() == true) {
+					System.out.println("");
+					System.out.println(myVirtualPetInstance.statusUpdate());
+				}
+				break;
+
+			}
+		}
+		if (myVirtualPetInstance.notDead() == false) {
+			System.out.print(
+					"\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
+			System.out.println("Ya blew it!\n             /\\\n" + "            \\ \\\n" + "             \\ \\\n"
+					+ "             / /\n" + "            / /\n" + "           _\\ \\_/\\/\\\n"
+					+ "          /  *  \\@@ =\n" + "         |       |Y/\n" + "         |       |~\n"
+					+ "          \\ /_\\ /\n" + "           \\\\ //\n" + "            |||\n" + "           _|||_\n"
+					+ "          ( / \\ ) \n" + myVirtualPetInstance.death());
+			System.exit(0);
+		}
+		input.close();
+//		System.out.println(" " + (myVirtualPetInstance.hunger) + " " + (myVirtualPetInstance.thirst) + " "
+//				+ (myVirtualPetInstance.waste) + " " + (myVirtualPetInstance.boredom) + " "
+//				+ (myVirtualPetInstance.tiredness));
 
 	}
-
 }
